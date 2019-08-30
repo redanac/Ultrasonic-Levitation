701d3c04899db79bb4f8583d38fa77f4bca6b78b
redanac <39522567+redanac@users.noreply.github.com>
2019-08-30 09:12:54 +0100

    Add files via upload

--- Pattern.py
+++ Pattern.py
@@ -5,12 +5,19 @@ Created on Sun Aug 25 22:35:03 2019
 @author: Redha
 """
 
+# Import stuff
+
 board = 1
 from Controller import *
 import Functions
 import pickle 
 import numpy as np
 from UltrasonicLevitator import *
+
+R=0.0725 # Radius of ring in m
+n=25 # Number of tds in a ring
+
+tdsys=ParticleSystem() #Initialise particle system
 
 for i in range(25): # 25 tds in each circle
     for theta in np.radians([72.5,67.5,61.5,55]): # angle of td along hemisphere face
@@ -34,9 +41,12 @@ for i in range(25): # 25 tds in each circle
         tdsys.appendTransducer(pos,-pos) # adds Transducer to system
         
 #Load in offsets for pattern
-offsets=pickle.load(open('opt_phi_ring.pkl'))
-
-tdsys.focus(Vector([0,0,0]))
+twin=np.array(pickle.load(open('opt_phi_ring.pkl','rb')))
+twin=twin%(2*np.pi)
+vortex=pickle.load(open('opt_phi_ring_vortex.pkl','rb'))
+antivort=vortex[::-1]
+                    
+tdsys.focus(Vector([0,0,0])) #Focuses td system at origin
 
 with Controller() as com:
     command = {"CMD":2}
@@ -48,29 +58,43 @@ with Controller() as com:
     reply = com.send_json(command_power)
     if reply["Status"] != "Success":
         raise Exception("Failed to start conversion", reply)
-        
-        
-    
-    choose=input('Press h for haptic \n or p for pattern')
-    
-    if choose == 'p':
-        
+                   
+    # Choose type of trap to generate    
+
+    print('\n \n t for twin trap')
+    print('v for votex trap \n \n')
+
+    choose=input('Select Trap \n\n')
+                   
+    if choose == 't':
+
+
+        print('twin', np.degrees(twin)-360 )
+
+
         for i in range(100):
             # Send offset commands
-            command = Functions.create_board_command_offset(board, i, offsets[i])
-            reply = com.send_json(command)
+            command = Functions.create_board_command_offset(board, i, twin[i]) # Sets offsets of each TD for twin trap
+            reply = com.send_json(command) #Checks if succesful
             if reply["Status"] != "Success":
                 raise Exception("Failed to start conversion", reply)
-        
-    if choose == 'h':
-        
-        for i in range(100):
-            # Send offset commands
-            command = Functions.create_board_command_offset(board, i, tdsys.transducers[i].phi)
-            reply = com.send_json(command)
-            if reply["Status"] != "Success":
-                raise Exception("Failed to start conversion", reply)
-                
+
+    elif choose =='v':
+
+        while 1:
+
+            for i in range(100):
+                # Send offset commands
+                command = Functions.create_board_command_offset(board, i, vortex[i])  # Sets offsets of each TD for vortex trap
+                reply = com.send_json(command)  #Checks if succesful
+                if reply["Status"] != "Success":
+                    raise Exception("Failed to start conversion", reply)
+            
+
+    else:
+        raise Exception('Please select an option from the above choices')
+    
+    
                 
     # Send load offset command
     command = Functions.create_board_command_load_offsets(board)
